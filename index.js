9ca31bb51cbdc662cc679e024ecee3adb5d81027
Auguste <auguste.tomaseviciute@fundingcircle.com>
2022-04-17 18:43:44 +0100

    requirement unclear 180degree change

--- index.js
+++ index.js
@@ -7,13 +7,23 @@ let ebay = new Ebay({
     headers: {
         // optional
         "X-EBAY-C-MARKETPLACE-ID": "EBAY_GB"
+    },
+    body: {
+        grant_type: 'client_credentials',
+        scope: 'https://api.ebay.com/oauth/api_scope'
+
     }
 });
+ebay.getAccessToken().then((data) => {
+    console.log(data); // data.access_token
+    ebay.setAppAccessToken(data.access_token);
+}, (error) => {
+    console.log(error);
+});
 
-ebay.findItemsAdvanced({
-    keywords: 'iphone'
-}).then((data) => {
+ebay.findItemsByCategory(10181).then((data) => {
     console.log(data[0].searchResult[0]);
 }, (error) => {
     console.log(error);
 });
+
